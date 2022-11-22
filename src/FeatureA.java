35677c173070470bcc0f4d6925af593bc0ff7162
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-11-22 15:14:02 +0100

    Added Name to FeatureA

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -3,6 +3,10 @@
 public class FeatureA{
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hallo, das ist der Code von <Name>");
+=======
+        System.out.println("Hallo, das ist der Code von Steffen");
+>>>>>>> f7191e8 (Added Name to FeatureA)
     }
 }
