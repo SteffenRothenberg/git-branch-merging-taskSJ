c722e3bf9b03abe3633d11668e046362c50af4a0
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2023-03-09 15:09:56 +0100

    Only sout

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -3,10 +3,6 @@
 public class FeatureA{
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hallo, das ist der Code von <Name>");
-=======
         System.out.println("Hallo, das ist der Code von Steffen");
->>>>>>> f7191e8 (Added Name to FeatureA)
     }
 }
